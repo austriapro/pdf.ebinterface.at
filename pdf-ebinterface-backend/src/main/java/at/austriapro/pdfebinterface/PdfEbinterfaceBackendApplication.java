c6f5c675b834857f417664c0aa5789301276cd74
Robert Gründler <robert@dubture.com>
2017-09-08 11:13:28 +0200

    initial project structure

---
+++ pdf-ebinterface-backend/src/main/java/at/austriapro/pdfebinterface/PdfEbinterfaceBackendApplication.java
@@ -0,0 +1,12 @@
+package at.austriapro.pdfebinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfEbinterfaceBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PdfEbinterfaceBackendApplication.class, args);
+  }
+}
