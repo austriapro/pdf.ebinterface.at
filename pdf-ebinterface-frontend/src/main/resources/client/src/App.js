2d9f88c83a58da6c37007c0f77988cccefa5b5f2
Pawel Kowalski <pawel.kowalski@ecosio.com>
2017-09-11 10:09:09 +0200

    render list of users

--- pdf-ebinterface-frontend/src/main/resources/client/src/App.js
+++ pdf-ebinterface-frontend/src/main/resources/client/src/App.js
@@ -3,7 +3,67 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      error: null,
+      loading: true,
+    };
+  }
+
+  componentDidMount(){
+    fetch("/api/dummies/")
+      .then(response => {
+        if (response.status === 200) {
+          response.json().then(users => {
+          console.log(users);
+            this.setState({
+              users: users,
+              loading: false,
+              error: null,
+            })
+          });
+        }
+      });
+  }
+
+/*  componentDidMount(){
+    let $component = this;
+    fetch("/api/dummies/")
+      .then(function(response) {
+        if (response.status === 200) {
+          response.json().then(function(users){
+            console.log(users);
+            $component.setState({
+              users: users,
+              loading: false,
+              error: null,
+            })
+          });
+        }
+        else{}
+      });
+  }*/
+
+  render(){
+    if (this.state.loading === true) {
+      return (
+        <div>Loading...</div>
+      )}
+
+    else if (this.state.loading === false) {
+      return (
+        <div>
+          {this.state.users.map(item => {
+            return (
+              <div key={item.uuid}>{JSON.stringify(item)}</div>
+            )
+          })}
+        </div>
+      )}
+
     return (
       <div className="App">
         <div className="App-header">
