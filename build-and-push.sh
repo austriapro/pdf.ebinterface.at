53891222ed362237a35a70042b539db677efafa0
Robert Gründler <robert@dubture.com>
2017-09-19 16:23:53 +0200

    MOVED docker image to hub.docker.com

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build -t pdf-to-ebinterface .
-docker tag pdf-to-ebinterface:latest 752277495799.dkr.ecr.eu-west-1.amazonaws.com/pdf-to-ebinterface:latest
-docker push 752277495799.dkr.ecr.eu-west-1.amazonaws.com/pdf-to-ebinterface:latest
+docker build -t ebinterface/pdf-to-ebinterface .
+docker tag pdf-to-ebinterface:latest ebinterface/pdf-to-ebinterface:latest
+docker push ebinterface/pdf-to-ebinterface:latest
