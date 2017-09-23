f8d92ed2c29f2bdb3bc934fce701c68037d43562
Philipp Liegl <philipp.liegl@ecosio.com>
2017-09-23 14:58:50 +0200

    ADDED support for additional REST operations to dummy service

--- pdf-ebinterface-backend/src/main/java/at/austriapro/pdfebinterface/controller/DummyController.java
+++ pdf-ebinterface-backend/src/main/java/at/austriapro/pdfebinterface/controller/DummyController.java
@@ -17,32 +17,46 @@ public class DummyController {
 
   private Random random = new Random();
 
-  private List<Dummy> dummies = new ArrayList<>();
+  //Dummy data store
+  private HashMap<String, Dummy> dummies = new HashMap<>();
 
 
   @PostConstruct
   public void init() {
-    dummies.add(Dummy.builder().name("foo").email("foo@example.com").uuid("1").build());
-    dummies.add(Dummy.builder().name("bar").email("bar@example.com").uuid("2").build());
+    dummies.put("1", Dummy.builder().name("foo").email("foo@example.com").uuid("1").build());
+    dummies.put("2", Dummy.builder().name("bar").email("bar@example.com").uuid("2").build());
+    dummies.put("3", Dummy.builder().name("foobar").email("foobar@example.com").uuid("3").build());
   }
 
+
+  /**
+   * Get a single one
+   * @param uuid
+   * @return
+   */
+  @GetMapping("/dummy/{uuid}")
+  public Dummy getDummy(@PathVariable String uuid) {
+    return dummies.get(uuid);
+  }
+
+
+  /**
+   * Get all
+   * @return
+   * @throws InterruptedException
+   */
   @GetMapping("/dummies")
   public List<Dummy> getDummies() throws InterruptedException {
-
-    int rd = random.nextInt(100);
-
-
-    // fake random failures.
-   // if (rd > 80) {
-    //  throw new RuntimeException("Internal Server Error");
-  //  }
-
-    // fake latency
-  //  Thread.sleep(2000);
-
-    return dummies;
+    return new ArrayList<>(dummies.values());
   }
 
+
+
+  /**
+   * Add a single one
+   * @param dummy
+   * @return
+   */
   @PostMapping("/dummy")
   public Dummy postDummy(@RequestBody Dummy dummy) {
 
@@ -51,23 +65,111 @@ public class DummyController {
     }
 
     dummy.setUuid(UUID.randomUUID().toString());
-    dummies.add(dummy);
+    dummies.put(dummy.getUuid(), dummy);
     return dummy;
   }
 
+  /**
+   * Add a bunch
+   * @param postedDummies
+   * @return
+   */
+  @PostMapping("/dummies")
+  public List<Dummy> postDummies(@RequestBody List<Dummy> postedDummies) {
+
+    List<Dummy> added = new ArrayList<>();
+    postedDummies.forEach((d) -> {
+      if (!dummies.containsKey(d.getUuid())) {
+        dummies.put(d.getUuid(), d);
+        added.add(d);
+      }
+    });
+
+    return added;
+
+  }
+
+
+  /**
+   * Delete a single one
+   * @param uuid
+   * @return
+   */
   @DeleteMapping("/dummy/{uuid}")
-  public void deleteDummy(@PathVariable String uuid) {
+  public String deleteDummy(@PathVariable String uuid) {
 
     if (uuid == null) {
-      throw new RuntimeException("Cannot create new dummy with existing uuid");
+      throw new RuntimeException("Cannot delete a dummy without a UUID");
     }
 
-    for (Iterator<? extends Dummy> it = dummies.iterator(); it.hasNext(); ) {
-      if (uuid.equals(it.next().uuid)) {
-        it.remove();
-        break;
-      }
+    Dummy d = dummies.remove(uuid);
+
+    if (d == null) {
+      return String.format("Unable to delete dummy %s. Dummy could not be found.", uuid);
+    }
+    else {
+      return String.format("Deleted dummy %s", uuid);
     }
   }
+
+  /**
+   * Delete a bunch
+   * @param dummyToDelete
+   * @return
+   */
+  @DeleteMapping("/dummies")
+  public List<Dummy> deleteDummies(@RequestBody List<Dummy> dummyToDelete) {
+
+      List<Dummy> deletedEntries = new ArrayList<>();
+
+      for (Dummy d : dummyToDelete) {
+        Dummy deletedEntry = dummies.remove(d.getUuid());
+        if (deletedEntry != null) {
+          deletedEntries.add(deletedEntry);
+        }
+      }
+
+      return deletedEntries;
+  }
+
+
+  /**
+   * Update a single one. If it does not exist - add it
+   * @param dummy
+   * @param uuid
+   * @return
+   */
+  @PutMapping("/dummy/{uuid}")
+  public Dummy updateDummy(@RequestBody Dummy dummy, @PathVariable String uuid) {
+
+    if (uuid == null) {
+      throw new RuntimeException("Cannot update a dummy without a UUID");
+    }
+    //UUID from path takes precendence over uuid in the request body
+    dummy.setUuid(uuid);
+    dummies.put(uuid, dummy);
+    return dummy;
+  }
+
+
+  /**
+   * Update a bunch. If an entry does not exist, add it.
+   * @param dummyList
+   * @return
+   */
+  @PutMapping("/dummies")
+  public List<Dummy> updateDummies(@RequestBody List<Dummy> dummyList) {
+
+    for (Dummy d : dummyList) {
+      dummies.put(d.getUuid(), d);
+    }
+
+    return dummyList;
+  }
+
+
+
+
+
 
 }
