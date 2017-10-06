2c13a7853cd0ab357809fff5a4e1aef15131384b
Robert Gründler <robert@dubture.com>
2017-10-06 10:14:12 +0200

    ADDED download functionality to backend

--- pdf-ebinterface-backend/src/main/java/at/austriapro/pdfebinterface/controller/ConverterController.java
+++ pdf-ebinterface-backend/src/main/java/at/austriapro/pdfebinterface/controller/ConverterController.java
@@ -1,27 +1,69 @@
 package at.austriapro.pdfebinterface.controller;
 
-    import org.springframework.http.ResponseEntity;
-    import org.springframework.web.bind.annotation.PostMapping;
-    import org.springframework.web.bind.annotation.RequestMapping;
-    import org.springframework.web.bind.annotation.RequestParam;
-    import org.springframework.web.bind.annotation.RestController;
-    import org.springframework.web.multipart.MultipartFile;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 
-    import java.util.UUID;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-    import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/convert")
 public class ConverterController {
 
+  private Cache<String, byte[]> files = CacheBuilder.newBuilder()
+      .maximumSize(1000)
+      .expireAfterWrite(10, TimeUnit.MINUTES)
+      .expireAfterAccess(10, TimeUnit.SECONDS)
+      .build();
+
   /**
    * curl -i -X POST -H "Content-Type: multipart/form-data" -F "name=the-file-name" -F "file=@testfile.xml" http://127.0.0.1:8080/api/convert
    */
   @PostMapping
-  public ResponseEntity<String> uploadFile(@RequestParam("name") String name, @RequestParam("file") MultipartFile file) {
-    // TODO
-    return ResponseEntity.ok(UUID.randomUUID().toString());
+  public UploadResponse uploadFile(@RequestParam("name") String name, @RequestParam("file") MultipartFile file)
+      throws IOException {
+
+    String uuid = UUID.randomUUID().toString();
+    log.info("Genarating pdf using uuid {}", uuid);
+    files.put(uuid, file.getBytes());
+    log.info("File cache contains {} items", files.size());
+    return new UploadResponse(uuid);
   }
+
+  /**
+   * curl -i http://127.0.0.1:8080/api/convert/{uuid}
+   */
+  @GetMapping("/{uuid}")
+  public Resource downloadFile(@PathVariable String uuid) {
+    log.info("Client retrieves pdf with uuid {}. File cache contains {} items.", uuid, files.size());
+    byte[] file = files.getIfPresent(uuid);
+
+    if (file == null) {
+      throw new RuntimeException("Unable to retrieve file with uuid " + uuid);
+    }
+
+    return new ByteArrayResource(file);
+  }
+
+  @Data
+  public static class UploadResponse implements Serializable {
+    private final String uuid;
+  }
+
 }
