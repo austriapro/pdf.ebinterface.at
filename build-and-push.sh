a3d0895bc2403ddd2387c9110b7d304f0b1c65eb
Robert Gründler <robert@dubture.com>
2017-10-13 14:45:10 +0200

    FIXED docker build

--- build-and-push.sh
+++ build-and-push.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+mvn clean package 
 docker build -t ebinterface/pdf-to-ebinterface .
 docker tag ebinterface/pdf-to-ebinterface:latest ebinterface/pdf-to-ebinterface:latest
 docker push ebinterface/pdf-to-ebinterface:latest
