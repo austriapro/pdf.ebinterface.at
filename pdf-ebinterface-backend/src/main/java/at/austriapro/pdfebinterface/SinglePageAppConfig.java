c48d8d1fed6703cc30ed3b7c4af1e1011a814e68
Philip Helger <philip@helger.com>
2021-03-26 12:13:30 +0100

    Reworked slightly

--- pdf-ebinterface-backend/src/main/java/at/austriapro/pdfebinterface/SinglePageAppConfig.java
+++ pdf-ebinterface-backend/src/main/java/at/austriapro/pdfebinterface/SinglePageAppConfig.java
@@ -1,90 +1,89 @@
 package at.austriapro.pdfebinterface;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.util.StringUtils;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.resource.ResourceResolver;
-import org.springframework.web.servlet.resource.ResourceResolverChain;
-
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.ResourceResolver;
+import org.springframework.web.servlet.resource.ResourceResolverChain;
+
 /**
  * Redirects every page to index.html Used to handle the router
  */
 @Configuration
-public class SinglePageAppConfig extends WebMvcConfigurerAdapter {
-
+public class SinglePageAppConfig implements WebMvcConfigurer {
   @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+  public void addResourceHandlers(final ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/**")
-      .addResourceLocations("classpath:/static/")
-      .resourceChain(false)
-      .addResolver(new PushStateResourceResolver());
+    .addResourceLocations("classpath:/static/")
+    .resourceChain(false)
+    .addResolver(new PushStateResourceResolver());
   }
 
   private class PushStateResourceResolver implements ResourceResolver {
 
-    private Resource index = new ClassPathResource("/static/index.html");
-    private List<String>
-      handledExtensions =
-      Arrays.asList("html", "js", "json", "csv", "css", "png", "svg", "eot", "ttf", "woff", "appcache", "jpg", "jpeg",
-                    "gif", "ico");
-    private List<String> ignoredPaths = Arrays.asList("api");
+    private final Resource index = new ClassPathResource("/static/index.html");
+    private final List<String>
+    handledExtensions =
+    Arrays.asList("html", "js", "json", "csv", "css", "png", "svg", "eot", "ttf", "woff", "woff2", "map", "appcache", "jpg", "jpeg",
+                  "gif", "ico");
+    private final List<String> ignoredPaths = Arrays.asList("api");
 
     @Override
-    public Resource resolveResource(HttpServletRequest request, String requestPath, List<? extends Resource> locations,
-                                    ResourceResolverChain chain) {
+    public Resource resolveResource(final HttpServletRequest request, final String requestPath, final List<? extends Resource> locations,
+                                    final ResourceResolverChain chain) {
       return resolve(requestPath, locations);
     }
 
     @Override
-    public String resolveUrlPath(String resourcePath, List<? extends Resource> locations, ResourceResolverChain chain) {
-      Resource resolvedResource = resolve(resourcePath, locations);
+    public String resolveUrlPath(final String resourcePath, final List<? extends Resource> locations, final ResourceResolverChain chain) {
+      final Resource resolvedResource = resolve(resourcePath, locations);
       if (resolvedResource == null) {
         return null;
       }
       try {
         return resolvedResource.getURL().toString();
-      } catch (IOException e) {
+      } catch (final IOException e) {
         return resolvedResource.getFilename();
       }
     }
 
-    private Resource resolve(String requestPath, List<? extends Resource> locations) {
+    private Resource resolve(final String requestPath, final List<? extends Resource> locations) {
       if (isIgnored(requestPath)) {
         return null;
       }
       if (isHandled(requestPath)) {
         return locations.stream()
-          .map(loc -> createRelative(loc, requestPath))
-          .filter(resource -> resource != null && resource.exists())
-          .findFirst()
-          .orElseGet(null);
+            .map(loc -> createRelative(loc, requestPath))
+            .filter(resource -> resource != null && resource.exists())
+            .findFirst()
+            .orElseGet(null);
       }
       return index;
     }
 
-    private Resource createRelative(Resource resource, String relativePath) {
+    private Resource createRelative(final Resource resource, final String relativePath) {
       try {
         return resource.createRelative(relativePath);
-      } catch (IOException e) {
+      } catch (final IOException e) {
         return null;
       }
     }
 
-    private boolean isIgnored(String path) {
+    private boolean isIgnored(final String path) {
       return ignoredPaths.contains(path);
     }
 
-    private boolean isHandled(String path) {
-      String extension = StringUtils.getFilenameExtension(path);
+    private boolean isHandled(final String path) {
+      final String extension = StringUtils.getFilenameExtension(path);
       return handledExtensions.stream().anyMatch(ext -> ext.equals(extension));
     }
   }
